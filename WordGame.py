ad16b51c54636b281af7c0bd6a643c9a2399dc6b
andrew <andlee@bu.edu>
2021-02-12 14:47:13 -0500

    Finalized random letter generation

--- WordGame.py
+++ WordGame.py
@@ -1,22 +1,24 @@
-from flask import Flask, request, session
-from flask import render_template
+from flask import Flask, request, session, redirect, render_template
 import requests
 import json
+import random 
 
 app = Flask(__name__)
 app.secret_key='secret'
 
-
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def home_page():
     #initiate session variables
     session['badGuess'] = -1
     session['hints'] = []
+    session['letterHints'] = []
+    session['gameStatus'] = ''
+    session['images'] = [] #Image links
     return render_template('home_page.html')
 
 @app.route('/image', methods = ['POST', 'GET'])
 def image_page():
-    isCorrect = ''
+    isCorrect = '' #Remove
     
     if request.method == 'POST':
         if request.form['randomWord'] == 'Random Word':
@@ -29,29 +31,38 @@ def image_page():
             print(session.get('imageURL'))
             print(session.get('imageWord'))
 
-            #Generate underscores to indicate word length
+            #Generate underscores to indicate word length and generate letter hints
             for i in range(len(session.get('imageWord'))):
                 session['hints'].append('_ ')
+                session['letterHints'].append(i)
 
         #Compare guess word to real query    
         guess = request.form.get("guess")
+        #Correct Guess
         if guess == session.get('imageWord'):
             isCorrect = 'Correct'
             session['hints'] = session.get('imageWord')
+            session['gameStatus'] = 'You won with ' + str(session.get('badGuess')) + ' tries'
+            return redirect('/gameover')
+        #Incorrect Guess
         else:
             session['badGuess'] += 1
             isCorrect = str(session.get('badGuess')) + ' tries made'
-            if session.get('badGuess') > 2:
-                #TODO: generate random letters
-                session['hints'][session.get('badGuess')-3] = session['imageWord'][session.get('badGuess')-3]
+            if session.get('badGuess') > 2 and session.get('badGuess') < (len(session.get('imageWord')) + 2): #Generate Random Letters
+                random.seed()
+                position = random.randrange(0, len(session.get('letterHints')))
+                letterPositionNum = session['letterHints'][position]
+                session['letterHints'].remove(letterPositionNum)
+                session['hints'][letterPositionNum] = session['imageWord'][letterPositionNum]
+            elif session.get('badGuess') >= (len(session.get('imageWord')) + 2):
+                session['gameStatus'] = 'You are a failure, the correct word was ' + session.get('imageWord')
+                return redirect('/gameover')
 
-
-        
-
-
-        
-        
     return render_template('image.html', image=session.get('imageURL'), correct=isCorrect, hints="".join(session['hints']))
+
+@app.route('/gameover')
+def gameover_page():
+    return render_template('gameover.html', status=session.get('gameStatus'))
 
 if __name__=='__main__':
     app.run()
